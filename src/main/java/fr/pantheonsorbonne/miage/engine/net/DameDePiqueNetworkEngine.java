1d9246c3dac784e91983183192ef0643cf584394
marlenaxynou <146637576+marlenaxynou@users.noreply.github.com>
2024-12-16 21:06:10 +0100

    Update DameDePiqueNetworkEngine.java

--- src/main/java/fr/pantheonsorbonne/miage/engine/net/DameDePiqueNetworkEngine.java
+++ src/main/java/fr/pantheonsorbonne/miage/engine/net/DameDePiqueNetworkEngine.java
@@ -1,5 +1,71 @@
 package fr.pantheonsorbonne.miage.engine.net;
 
-public class DameDePiqueNetworkEngine {
-    
+import fr.pantheonsorbonne.miage.Facade;
+import fr.pantheonsorbonne.miage.HostFacade;
+import fr.pantheonsorbonne.miage.engine.DameDePiqueGameEngine;
+import fr.pantheonsorbonne.miage.exception.NoMoreCardException;
+import fr.pantheonsorbonne.miage.game.Card;
+import fr.pantheonsorbonne.miage.game.Deck;
+import fr.pantheonsorbonne.miage.game.Player;
+import fr.pantheonsorbonne.miage.model.Game;
+import fr.pantheonsorbonne.miage.model.GameCommand;
+
+import java.util.*;
+
+/**
+ * This class implements the Dame de Pique game with the network engine
+ */
+public class DameDePiqueNetworkEngine extends DameDePiqueGameEngine {
+    private static final int PLAYER_COUNT = 4; // Assuming 4 players are needed
+    private final Game dameDePiqueGame;
+
+    public DameDePiqueNetworkEngine(Deck deck, HostFacade hostFacade, Set<String> players, Game dameDePiqueGame) {
+        super(deck, hostFacade);
+        this.dameDePiqueGame = dameDePiqueGame;
+    }
+
+    public static void main(String[] args) {
+        // Create the host facade
+        HostFacade hostFacade = Facade.getFacade();
+        hostFacade.waitReady();
+
+        // Set the name of the host player
+        hostFacade.createNewPlayer("Host");
+
+        // Create a new game of Dame de Pique
+        Game dameDePiqueGame = hostFacade.createNewGame("DAME_DE_PIQUE");
+
+        // Wait for enough players to join
+        hostFacade.waitForExtraPlayerCount(PLAYER_COUNT);
+
+        DameDePiqueNetworkEngine host = new DameDePiqueNetworkEngine(new StandardDeck(), hostFacade, new HashSet<>(dameDePiqueGame.getPlayers()), dameDePiqueGame);
+        host.play();
+        System.exit(0);
+    }
+
+    /**
+     * Get the set of players initially in the game
+     */
+    @Override
+    protected List<String> getInitialPlayers() {
+        return new ArrayList<>(this.dameDePiqueGame.getPlayers());
+    }
+
+    @Override
+    protected void playTurn(Player player) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'playTurn'");
+    }
+
+    @Override
+    protected boolean isGameOver() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'isGameOver'");
+    }
+
+    @Override
+    protected void declareWinner(Player winner) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'declareWinner'");
+    }
 }
