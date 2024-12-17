85ac9cf9c6a4ed7a8bda01eb72354876364331d4
marlenaxynou <146637576+marlenaxynou@users.noreply.github.com>
2024-12-17 14:43:56 +0100

    Update RandomDeck.java

--- src/main/java/fr/pantheonsorbonne/miage/game/RandomDeck.java
+++ src/main/java/fr/pantheonsorbonne/miage/game/RandomDeck.java
@@ -2,6 +2,9 @@ package fr.pantheonsorbonne.miage.game;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
+
+import fr.pantheonsorbonne.miage.exception.NoMoreCardException;
 
 public class RandomDeck extends Deck {
 
@@ -14,5 +17,14 @@ public class RandomDeck extends Deck {
             hands.add(cards.subList(i * cardsPerPlayer, (i + 1) * cardsPerPlayer).toArray(new Card[0]));
         }
         return hands;
+    }
+
+    @Override
+    public Card drawCard() throws NoMoreCardException {
+        if (cards.isEmpty()) {
+            throw new NoMoreCardException();
+        }
+        Random random = new Random();
+        return cards.remove(random.nextInt(cards.size()));
     }
 }
